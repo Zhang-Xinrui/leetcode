18e9ec81c69662cebec9282645d47fffcd64ba6f
zxr <zxr001119@mail.ustc.edu.cn>
2020-02-21 11:37:52 +0800

    remove useless things

--- demo_try/stringfor.java
+++
@@ -1,9 +0,0 @@
-public class stringfor {
-    public static void main(String[] args) {
-        String[] a = {"zxr", "yjh"};
-        for(String b : a) {
-            b = b + "a";
-            System.out.println(a[0] + a[1]);
-        }
-    }
-}
--- lc1.java
+++
@@ -1,5 +0,0 @@
-class lc1 {
-    public static void main(String[] args){
-        System.out.print(1);
-    }
-}
